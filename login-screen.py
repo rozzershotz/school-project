fe17dbb928a76037c9a81e5668c303fef07f4612
rozzer <60936073+rozzershotz@users.noreply.github.com>
2023-11-13 21:01:01 +0000

    added database. fixed issues with class and functions

--- login-screen.py
+++ login-screen.py
@@ -2,6 +2,14 @@ from tkinter import *
 import tkinter.font as TkFont
 mainfont = ("/home/pi/Documents/q13rtaylor-project/school-project/fonts/american_captain/American Captain.ttf")
 
+def makeDatabase(db):
+    c = db.cursor()
+    c.execute("DROP TABLE IF EXISTS username")
+    c.execute("DROP TABLE IF EXISTS password")
+    c.execute("CREATE TABLE password")
+    c.execute("CREATE TABLE username (userID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)")
+    db.commit()
+
 # initiated Class, created parameters for screens
 class App(Tk):
     def __init__(self):
@@ -11,7 +19,7 @@ class App(Tk):
         self.titleFont = TkFont.Font(family="Arial", size=30, weight="bold")
         self.buttonFont = TkFont.Font(family="Arial", size=15, weight="bold")
 
-
+    def run(self):
         # first screen
         self.firstFrame = Frame(self, width=1920, height=1080)
         self.title = Label(self.firstFrame, anchor="center", text="Battleships", bg="#0074b7", fg="white", font=self.titleFont)
@@ -22,28 +30,23 @@ class App(Tk):
         spacing = Label(self.firstFrame, text="", height="12", width="50")
         spacing.grid(row=1, column=0)
 
-        # added interactive hover on buttons
-        def changeOnHover(Button, colorOnHover, colorOnLeave):
-            Button.bind("<Enter>", func=lambda e: Button.config(background=colorOnHover))
-    
-            Button.bind("<Leave>", func=lambda e: Button.config(background=colorOnLeave))
-
         # creating buttons
         loginButton = Button(self.firstFrame, text="Login", height="5", width="50", activebackground="#055c9d", activeforeground="white", command = self.loginSwitch)
         loginButton.grid(row=2,column=0)
-        changeOnHover(loginButton, "#0074b7", "white")
+        self.changeOnHover(loginButton, "#0074b7", "white")
 
         self.firstFrame.rowconfigure(3,minsize=1)
 
         registerButton = Button(self.firstFrame, text="Register", height="5", width="50", activebackground="#055c9d", activeforeground="white", command=self.RegisterSwitch)
         registerButton.grid(row=4,column=0)
-        changeOnHover(registerButton, "#0074b7", "white")
+        self.changeOnHover(registerButton, "#0074b7", "white")
 
         self.firstFrame.rowconfigure(5,minsize=1)
 
         guestButton = Button(self.firstFrame, text="Guest", height="5", width="50", activebackground="#055c9d", activeforeground="white")
         guestButton.grid(row=6,column=0)
-        changeOnHover(guestButton, "#0074b7", "white")
+        self.changeOnHover(guestButton, "#0074b7", "white")
+
 
         # creating other screens
         self.loginFrame = Frame(self, width=1920, height=1080)
@@ -55,10 +58,7 @@ class App(Tk):
         self.loginFrame.rowconfigure(1,minsize=1)
 
         # back button
-        backButton = Button(self.loginFrame, text="Back", height="2", width="10", activebackground="gray", activeforeground="white", command=self.BackSwitch)
-        backButton.grid(row=2, column=0)
-        changeOnHover(backButton, "ADD8E6", "white")
-
+        self.addBackButton(self.loginFrame)
 
         self.registerFrame = Frame(self, width=1920, height=1080)
         self.registerTitle = Label(self.registerFrame, anchor="center", text="Register", bg="#116530", fg="white", font=self.titleFont)
@@ -68,11 +68,19 @@ class App(Tk):
 
         self.registerFrame.rowconfigure(1,minsize=1)
 
-        backButton = Button(self.registerFrame, text="Back", height="2", width="10", activebackground="gray", activeforeground="white", command=self.BackSwitch)
-        backButton.grid(row=2, column=0)
-        changeOnHover(backButton, "ADD8E6", "white")
+        self.addBackButton(self.registerFrame)
 
         self.mainloop()
+
+    # added interactive hover on buttons
+    def changeOnHover(self, Button, colorOnHover, colorOnLeave):
+        Button.bind("<Enter>", func=lambda e: Button.config(background=colorOnHover))
+        Button.bind("<Leave>", func=lambda e: Button.config(background=colorOnLeave))
+
+    def addBackButton(self, frameRef):
+        backButton = Button(frameRef, text="Back", height="2", width="10", activebackground="gray", activeforeground="white", command=self.BackSwitch)
+        backButton.grid(row=2, column=0)
+        self.changeOnHover(backButton, "ADD8E6", "white")
 
     # creating way to switch between screens
     def loginSwitch(self):
@@ -88,4 +96,5 @@ class App(Tk):
         self.registerFrame.grid_forget()
         self.firstFrame.grid(row=0, column=0, rowspan=3, sticky="NSEW")
 
-App()
+app = App()
+app.run()

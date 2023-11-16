1f422586fbb2ed56d9c1f5d521d56f4edf7d5915
reuben <rozzershotz@gmail.com>
2023-11-16 12:43:23 +0000

    made game screen, linked to main menu in old class

---
+++ gameScreen.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import tkinter.font as TkFont
+
+class Game(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self)
+        self.titleFont = TkFont.Font(family="Arial", size=30, weight="bold")
+        self.buttonFont = TkFont.Font(family="Arial", size=15, weight="bold")
+
+        self.title = Label(self, anchor="center", text="hello new game", bg="#0074b7", fg="white", font = self.titleFont)
+        self.title.grid(row=0, column=0, sticky="NSEW", columnspan=2)
+        self.columnconfigure(0,weight=1)
+        self.rowconfigure(0,minsize=100)
+
+        spacing = Label(self, text="", height="12", width="50")
+        spacing.grid(row=1, column=0, columnspan=2)
--- loginScreen.py
+++ loginScreen.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import tkinter.font as TkFont
+from gameScreen import Game
 mainfont = ("/home/pi/Documents/q13rtaylor-project/school-project/fonts/american_captain/American Captain.ttf")
 
 def makeDatabase(db):
@@ -21,6 +22,7 @@ class App(Tk):
 
     def run(self):
         # first screen
+        self.gamescreen = Game(self)
         self.firstFrame = Frame(self, width=1920, height=1080)
         self.title = Label(self.firstFrame, anchor="center", text="Battleships", bg="#0074b7", fg="white", font=self.titleFont)
         self.title.grid(row=0, column=0, sticky="NSEW", columnspan=2)
@@ -30,33 +32,30 @@ class App(Tk):
         spacing = Label(self.firstFrame, text="", height="12", width="50")
         spacing.grid(row=1, column=0, columnspan=2)
 
-        exitButton = Button(self.firstFrame, text="Exit", height="2", width="10", command=self.destroy)
-        exitButton.grid(row=0,column=1, sticky="N")
-
         self.addExitButton(self.firstFrame)
 
 
         # creating buttons
-        loginButton = Button(self.firstFrame, text="Login", height="5", width="50", activebackground="#055c9d", activeforeground="white", command = self.loginSwitch)
-        loginButton.grid(row=2,column=0)
+        loginButton = Button(self.firstFrame, text="Login", height="5", width="50", background = "white", activebackground="#055c9d", activeforeground="white", command = self.loginSwitch)
+        loginButton.grid(row=2,column=0, columnspan=2)
         self.changeOnHover(loginButton, "#0074b7", "white")
 
         self.firstFrame.rowconfigure(3,minsize=1)
 
-        registerButton = Button(self.firstFrame, text="Register", height="5", width="50", activebackground="#055c9d", activeforeground="white", command=self.RegisterSwitch)
-        registerButton.grid(row=4,column=0)
+        registerButton = Button(self.firstFrame, text="Register", height="5", width="50", background = "white", activebackground="#055c9d", activeforeground="white", command=self.RegisterSwitch)
+        registerButton.grid(row=4,column=0, columnspan=2)
         self.changeOnHover(registerButton, "#0074b7", "white")
 
         self.firstFrame.rowconfigure(5,minsize=1)   
 
-        guestButton = Button(self.firstFrame, text="Guest", height="5", width="50", activebackground="#055c9d", activeforeground="white")
-        guestButton.grid(row=6,column=0)
+        guestButton = Button(self.firstFrame, text="Guest", height="5", width="50", background = "white", activebackground="#055c9d", activeforeground="white", command = self.mainMenuSwitch)
+        guestButton.grid(row=6,column=0, columnspan=2)
         self.changeOnHover(guestButton, "#0074b7", "white")
 
 
         # creating other screens
         self.loginFrame = Frame(self, width=1920, height=1080)
-        self.loginTitle= Label(self.loginFrame, anchor="center", text="Login", bg="#116530", fg="white", font=self.titleFont)
+        self.loginTitle = Label(self.loginFrame, anchor="center", text="Login", bg="#116530", fg="white", font=self.titleFont)
         self.loginTitle.grid(row=0, column=0, sticky="NSEW", columnspan=2)
         self.loginFrame.columnconfigure(0,weight=1)
         self.loginFrame.rowconfigure(0,minsize=100)
@@ -77,6 +76,31 @@ class App(Tk):
         self.addExitButton(self.registerFrame)
         self.addBackButton(self.registerFrame)
 
+        self.mainMenuFrame = Frame(self, width=1920, height=1080)
+        self.mainTitle = Label(self.mainMenuFrame, anchor="center", text="Main Menu", bg="#116530", fg="white", font=self.titleFont)
+        self.mainTitle.grid(row=0, column=0, sticky="NSEW", columnspan=2)
+        self.mainMenuFrame.columnconfigure(0,weight=1)
+        self.mainMenuFrame.rowconfigure(0,minsize=100)
+        spacing2 = Label(self.mainMenuFrame, text="", height="10", width="50")
+        spacing2.grid(row=2, column=0, columnspan=2)
+
+        playButton = Button(self.mainMenuFrame, text="Play", height="5", width="50", background = "white", activebackground="#055c9d", activeforeground="white", command = self.launchGame)
+        playButton.grid(row=3,column=0, columnspan=2)
+        self.changeOnHover(playButton, "#0074b7", "white")
+
+        achievementButton = Button(self.mainMenuFrame, text="Achievements", height="5", width="50", background = "white", activebackground="#055c9d", activeforeground="white")
+        achievementButton.grid(row=4,column=0, columnspan=2)
+        self.changeOnHover(achievementButton, "#0074b7", "white")
+
+        settingsButton = Button(self.mainMenuFrame, text="Settings", height="5", width="50", background = "white", activebackground="#055c9d", activeforeground="white")
+        settingsButton.grid(row=5,column=0, columnspan=2)
+        self.changeOnHover(settingsButton, "#0074b7", "white")
+
+        self.mainMenuFrame.rowconfigure(1,minsize=1)
+
+        self.addExitButton(self.mainMenuFrame)
+        self.addBackButton(self.mainMenuFrame)
+
         self.mainloop()
 
     # added interactive hover on buttons
@@ -85,12 +109,12 @@ class App(Tk):
         Button.bind("<Leave>", func=lambda e: Button.config(background=colorOnLeave))
 
     def addBackButton(self, frameRef):
-        backButton = Button(frameRef, text="Back", height="2", width="10", activebackground="gray", activeforeground="white", command=self.BackSwitch)
-        backButton.grid(row=2, column=0, columnspan=2)
+        backButton = Button(frameRef, text="Back", height="2", width="10", background = "white", activebackground="gray", activeforeground="white", command=self.BackSwitch)
+        backButton.grid(row=1, column=0, columnspan=2)
         self.changeOnHover(backButton, "ADD8E6", "white")
 
     def addExitButton(self, frameRef):
-        exitButton = Button(frameRef, text="Quit", height="2", width="10", activebackground="gray", activeforeground="white", command=self.destroy)
+        exitButton = Button(frameRef, text="Quit", height="2", width="10", background = "white", activebackground="gray", activeforeground="white", command=self.destroy)
         exitButton.grid(row=0, column=1)
         self.changeOnHover(exitButton, "ADD8E6", "white")
 
@@ -106,7 +130,17 @@ class App(Tk):
     def BackSwitch(self):
         self.loginFrame.grid_forget()
         self.registerFrame.grid_forget()
+        self.mainMenuFrame.grid_forget()
         self.firstFrame.grid(row=0, column=0, rowspan=3, columnspan=2, sticky="NSEW")
+
+    def mainMenuSwitch(self):
+        self.firstFrame.grid_forget()
+        self.mainMenuFrame.grid(row=0, column=0, rowspan=3, columnspan=2, sticky="NSEW")
+
+    def launchGame(self):
+        self.mainMenuFrame.grid_forget()
+        self.gamescreen.grid(row=0, column=0, rowspan=3, columnspan=2, sticky="NSEW")
+
 
 app = App()
 app.run()
